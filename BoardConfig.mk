Topaz-Development/device_xiaomi_topaz

topaz: Inherit common partition reserved sizes
Change-Id: I444ce2fcd37cdfa58d83a7e872dc547fb05a2a89
Signed-off-by: Coptan99 <omarcoptan9@gmail.com>

BoardConfig.mk
@@ -137,6 +137,7 @@ PRODUCT_COPY_FILES += \
 BOARD_USES_METADATA_PARTITION := true
 
 # Partitions
+-include vendor/lineage/config/BoardConfigReservedSize.mk
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x8000000
 BOARD_DTBOIMG_PARTITION_SIZE := 0x1800000
 BOARD_INIT_BOOT_IMAGE_PARTITION_SIZE := 0x800000

LineageOS-Geeky/android_device_xiaomi_topaz

BoardConfig.mk
@@ -158,6 +158,8 @@ TARGET_COPY_OUT_SYSTEM_EXT := system_ext
 TARGET_COPY_OUT_VENDOR := vendor
 TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
 
+-include vendor/aosp/config/BoardConfigReservedSize.mk
+
 # Platform
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := bengal
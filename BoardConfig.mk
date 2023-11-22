f2e5dbc902cddb54bd1b2eb072e72958174c1d36
Michael Bestas <mkbestas@lineageos.org>
2023-11-22 15:51:28 +0200

    topaz: Inherit common partition reserved sizes
    Change-Id: I444ce2fcd37cdfa58d83a7e872dc547fb05a2a89

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -158,6 +158,8 @@ TARGET_COPY_OUT_SYSTEM_EXT := system_ext
 TARGET_COPY_OUT_VENDOR := vendor
 TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
 
+-include vendor/aosp/config/BoardConfigReservedSize.mk
+
 # Platform
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := bengal
