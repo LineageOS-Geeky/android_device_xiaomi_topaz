68067401bc220f1767f281bd1ed8b433d905c871
Kizziama <kizziama@proton.me>
2024-12-22 13:35:21 +0000

    topaz: Return to QTI Health AIDL
    Change-Id: Ie20758cd509efa3ebcd37646ffbd94f3a1c937e8

--- device.mk
+++ device.mk
@@ -159,8 +159,8 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health-service.xiaomi \
-    android.hardware.health-service.xiaomi_recovery
+    android.hardware.health-service.qti \
+    android.hardware.health-service.qti_recovery
 
 # Hotword enrollment
 PRODUCT_COPY_FILES += \
